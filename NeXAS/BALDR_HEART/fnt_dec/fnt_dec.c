fc7dec3e1d21fde31eeb422ad6bef7a2f651ce3a
Darkness-TX <1545492547@qq.com>
2016-12-09 16:46:46 +0800

    完成fnt2png

--- NeXAS/BALDR_HEART/fnt_dec/fnt_dec.c
+++ NeXAS/BALDR_HEART/fnt_dec/fnt_dec.c
@@ -7,6 +7,7 @@
 #include <Windows.h>
 #include <zlib.h>
 #include <locale.h>
+#include <png.h>
 
 typedef unsigned char  unit8;
 typedef unsigned short unit16;
@@ -24,10 +25,59 @@ struct header
 	unit32 decompsize;
 }fnt_header;
 
+struct Font_Info
+{
+	unit32 unk;
+	unit16 width;
+	unit16 height;
+	unit32 offset;
+}font_info[10000];
+
+unit32 font_count = 0;
+
+void WritePng(FILE *pngfile, unit32 width, unit32 height, unit8* data)
+{
+	png_structp png_ptr;
+	png_infop info_ptr;
+	unit32 i = 0, k = 0;
+	unit8 *dst, *src;
+	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (png_ptr == NULL)
+	{
+		printf("PNG信息创建失败!\n");
+		exit(0);
+	}
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == NULL)
+	{
+		printf("info信息创建失败!\n");
+		png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+		exit(0);
+	}
+	png_init_io(png_ptr, pngfile);
+	png_set_IHDR(png_ptr, info_ptr, width, height, 8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+	png_write_info(png_ptr, info_ptr);
+	dst = malloc(width*height * 4);
+	src = data;
+	for (i = 0, k = 0; i < width*height * 2; i += 2, k++)
+	{
+		dst[k * 4 + 0] = src[i];
+		dst[k * 4 + 1] = src[i];
+		dst[k * 4 + 2] = src[i];
+		dst[k * 4 + 3] = 0xFF;//src[i + 1]
+	}
+	for (i = 0; i < height; i++)
+		png_write_row(png_ptr, dst + i*width * 4);
+	png_write_end(png_ptr, info_ptr);
+	png_destroy_write_struct(&png_ptr, &info_ptr);
+	free(dst);
+}
+
 void ReadIndex(char *fname)
 {
 	FILE *src, *dst;
 	unit8 *cdata, *udata, dstname[200];
+	unit32 i, savepos;
 	sprintf(dstname, "%s_INDEX", fname);
 	src = fopen(fname, "rb");
 	fread(fnt_header.magic, 1, 4, src);
@@ -49,22 +99,74 @@ void ReadIndex(char *fname)
 		printf("flag不为1!");
 		exit(0);
 	}
-	if (fnt_header.fontflag == 0x101)
+	if (fnt_header.fontflag == 0x103)
 	{
-		fread(&fnt_header.width, 1, 4, src);
-		fread(&fnt_header.height, 1, 4, src);
+		while (fgetc(src) != '\0')
+		{
+
+		}
+	}
+	fread(&fnt_header.width, 1, 4, src);
+	fread(&fnt_header.height, 1, 4, src);
+	fread(&fnt_header.decompsize, 1, 4, src);
+	if (fnt_header.decompsize == 0xFFFF00)
+	{
+		fseek(src, 9, SEEK_CUR);
 		fread(&fnt_header.decompsize, 1, 4, src);
-		fread(&fnt_header.compsize, 1, 4, src);
-		printf("width:%d height%d decompsize:0x%X compsize:0x%X\n", fnt_header.width, fnt_header.height, fnt_header.decompsize, fnt_header.compsize);
-		cdata = malloc(fnt_header.compsize);
-		udata = malloc(fnt_header.decompsize);
-		fread(cdata, 1, fnt_header.compsize, src);
-		uncompress(udata, &fnt_header.decompsize, cdata, fnt_header.compsize);
-		dst = fopen(dstname, "wb");
-		fwrite(udata, 1, fnt_header.decompsize, dst);
-		free(udata);
-		free(cdata);
-		fclose(dst);
+	}
+	fread(&fnt_header.compsize, 1, 4, src);
+	printf("width:%d height%d decompsize:0x%X compsize:0x%X\n", fnt_header.width, fnt_header.height, fnt_header.decompsize, fnt_header.compsize);
+	cdata = malloc(fnt_header.compsize);
+	udata = malloc(fnt_header.decompsize);
+	fread(cdata, 1, fnt_header.compsize, src);
+	uncompress(udata, &fnt_header.decompsize, cdata, fnt_header.compsize);
+	dst = fopen(dstname, "wb");
+	fwrite(udata, 1, fnt_header.decompsize, dst);
+	free(cdata);
+	fclose(dst);
+	if (strncmp(fname, "system16.fnt", 12) == 0)
+	{
+		font_count = fnt_header.decompsize / 0xC;
+		for (i = 0; i < font_count; i++)
+			memcpy(&font_info[i], &udata[i * 0xC], 0xC);
+	}
+	else if (strncmp(fname, "system24.fnt", 12) == 0)
+	{
+		font_count = fnt_header.decompsize / 0x10;
+		for (i = 0; i < font_count; i++)
+		{
+			memcpy(&font_info[i].offset, &udata[i * 0x10 + 0xC], 4);
+			memcpy(&font_info[i].width, &udata[i * 0x10 + 0x4], 2);
+			memcpy(&font_info[i].height, &udata[i * 0x10 + 0x6], 2);
+			memcpy(&font_info[i].unk, &udata[i * 0x10], 4);
+		}
+	}
+	printf("font_count:%d\n", font_count);
+	system("pause");
+	free(udata);
+	savepos = ftell(src);
+	sprintf(dstname, "%s_unpack", fname);
+	_mkdir(dstname);
+	_chdir(dstname);
+	for (i = 1; i < font_count; i++)
+	{
+		printf("\t%08d.png unk:0x%X width:%d height:%d offset:0x%X\n", i, font_info[i].unk, font_info[i].width, font_info[i].height, font_info[i].offset);
+		if (font_info[i].width != 0 && font_info[i].height != 0)
+		{
+			fseek(src, font_info[i].offset + savepos, SEEK_SET);
+			cdata = malloc(font_info[i].width*font_info[i].height * 2);
+			fread(cdata, 1, font_info[i].width*font_info[i].height * 2, src);
+			sprintf(dstname, "%08d.png", i);
+			dst = fopen(dstname, "wb");
+			WritePng(dst, font_info[i].width, font_info[i].height, cdata);
+			fclose(dst);
+			free(cdata);
+		}
+		else
+		{
+			sprintf(dstname, "%08d.png", i);
+			dst = fopen(dstname, "wb");
+		}
 	}
 	fclose(src);
 }
