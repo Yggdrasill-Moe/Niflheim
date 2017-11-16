c03ad90b589b78f3429406579dafdb5ddeed5076
Darkness-TX <1545492547@qq.com>
2017-11-16 19:52:34 +0800

    增加key_def.bin用于处理def.rld

--- ExHIBIT/rld_enc.py
+++ ExHIBIT/rld_enc.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-#用于加密rld文件，def.rld文件用的疑似另一组密钥，未解决
+#用于加密rld文件，def.rld用一组密钥，其他文件用另一组
 #by Darkness-TX
 #2017.11.16
 
@@ -17,7 +17,10 @@ def ReadKey(fname):
 	return key_list
 
 def main():
-	key_list = ReadKey('key.bin')
+	other_key_list = ReadKey('key.bin')
+	def_key_list = ReadKey('key_def.bin')
+	other_xor_key = 0x16E854ED
+	def_xor_key = 0xAE85A916
 	for f in os.listdir('rld_chs'):
 		if not f.endswith('.bin'):
 			continue
@@ -32,11 +35,17 @@ def main():
 		#最大只解密去掉文件头的前0xFFC0字节，后面的都没加密，原样输出即可
 		if j > 0x3ff0:
 			j = 0x3ff0
+		if f[:-4] == 'def':
+			key_list = def_key_list
+			xor_key = def_xor_key
+		else:
+			key_list = other_key_list
+			xor_key = other_xor_key
 		for k in range(0, j):
 			en_temp = struct.unpack('L', src.read(4))
 			key_count = k
 			key_count = key_count & 0xff
-			temp_key = key_list[key_count] ^ 0x16E854ED
+			temp_key = key_list[key_count] ^ xor_key
 			de_temp = en_temp[0] ^ temp_key
 			dst.write(struct.pack('L', de_temp))
 		#解密后的其他部分原样输出，除了上面注释的原因外，不可能每个文件的大小都能整除4的嘛，经测试最后那不足4字节的数据没加密
